5ccd319f09920304646b5ae98d0707fd02198037
jdidi422 <rassiljdidi3@gmail.com>
2024-12-13 20:26:43 +0100

    chnage

--- sort.h
+++ sort.h
@@ -12,9 +12,9 @@
  */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
 
 
@@ -26,6 +26,7 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 listint_t *swap(listint_t *node, listint_t **list);
+
 
 #endif
 
