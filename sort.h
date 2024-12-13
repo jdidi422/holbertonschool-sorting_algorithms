502f2ad4fed48c1a1c0afbc37d03882fc7d0c64f
jdidi422 <rassiljdidi3@gmail.com>
2024-12-13 20:14:34 +0100

    chnage

--- sort.h
+++ sort.h
@@ -29,7 +29,6 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 
 
-void print_array(const int *array, size_t size);
-void bubble_sort(int *array, size_t size);
+
 #endif
 
