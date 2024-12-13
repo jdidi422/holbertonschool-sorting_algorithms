ff833d79acf65b6e136ed74f51d81fd71ed8c32e
jdidi422 <rassiljdidi3@gmail.com>
2024-12-13 20:16:07 +0100

    chnage

--- sort.h
+++ sort.h
@@ -1,13 +1,8 @@
-#ifndef SORT_H
-#define SORT_H
-
+#ifndef sort
+#define sort
+#include "sort.h"
 #include <stdlib.h>
-#include <stddef.h>
-#include <string.h>
-#include <unistd.h>
 #include <stdio.h>
-#include <fcntl.h>
-
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -24,11 +19,13 @@ typedef struct listint_s
 
 
 void print_array(const int *array, size_t size);
-
+void print_list(const listint_t *list);
 
 void bubble_sort(int *array, size_t size);
-
-
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+listint_t *swap(listint_t *node, listint_t **list);
 
 #endif
 
